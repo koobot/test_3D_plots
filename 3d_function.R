e4bbaf690fc9e8cfdcdbdef113c72e4c24ada14d
koobot <the.koo.is.tall@gmail.com>
2022-04-15 21:17:47 +1000

    fix constraint

--- 3d_function.R
+++ 3d_function.R
@@ -1,6 +1,5 @@
 # TODO:
-#   Make adding rows to dataframe more efficient
-#   Make colour gradient so easier to see change
+#   Fix constraint
 
 # Adapted from https://r-graph-gallery.com/3d-surface-plot.html
 
@@ -29,6 +28,43 @@ tryCatch(
 library(rgl)
 library(tidyverse)
 
+
+# Plot function as plane ---------------------
+# Using this eliminates the dataframe!!!!!!
+x <- seq(-2, 2, by = 0.1)
+y <- seq(-5, 5, by = 0.1)
+
+
+# f(x,y) = 8x^2 - 2y
+f_xy <- function(x, y) {
+  k <- 8*x^2 - 2*y
+  #k <- x^2 + y^2 # known x^2 + y^2
+  return(k)
+}
+z <- outer(x, y, f_xy) # Matrix of k's for all x,y pairs
+
+# Plot f(x,y)
+p <- rgl::persp3d(x, y, z, col = "blue", alpha = 0.7)
+planes3d(a = 0, b = 0, c = 1, col = "green", alpha = 0.5)
+clipplanes3d(a = 0, b = 0, c = 1) # Optional clip
+
+# Plot x^2 + y^2
+# Need to use polar coordinates: https://stackoverflow.com/a/51908543
+r <- 1
+phi <- seq(0, 2*pi, by = 10)
+x2 <- r*cos(phi)
+y2 <- r*sin(phi)
+plot(x2, y2)
+
+z2 <- outer(x2, y2, function(x,y) x^2 + y^2) # Doesn't include negatives!!
+
+p
+rgl::persp3d(x = x2, y = z2, z = z2, col = "yellow", alpha = 0.5, add = T)
+clipplanes3d(a = 0, b = 0, c = 1) # Optional clip
+
+
+# OLD CODE FOR SCATTER PLOT ----------------------
+
 # input
 u <- seq(-2, 2, by = 0.1)
 v <- seq(-5, 5, by = 0.1)
@@ -50,18 +86,10 @@ coord <- coord %>%
   rename(x = Var1, y = Var2) %>%
   mutate(z = f_xy(x, y))
 
-# plot scatter plot ----------------------
+# plot scatter plot
 p <- rgl::plot3d(coord$x, coord$y, coord$z, type = "s", radius = 0.5, col = "grey")
 planes3d(a = 0, b = 0, c = 1, col = "blue", alpha = 0.5)
 
 # Run with this option to clip the plane!!! Very cool!!!
 p
 clipplanes3d(a = 0, b = 0, c = 1)
-
-# Plot function as plane ---------------------
-# Using this eliminates the dataframe!!!!!!
-k <- outer(u, v, f_xy) # Matrix of coordinates
-
-rgl::persp3d(x = u, y = v, z = k, col = "grey")
-planes3d(a = 0, b = 0, c = 1, col = "blue", alpha = 0.5)
-clipplanes3d(a = 0, b = 0, c = 1) # Optional clip
